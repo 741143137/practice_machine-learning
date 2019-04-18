52611607f54dc8c2f026f78169c370a86ac65ac1
Mac for Jax <xuan.wang2016@hotmail.com>
2019-04-17 23:10:41 -0400

    Week 8

---
+++ clustering and PCA/kMeansInitCentroids.m
@@ -0,0 +1,29 @@
+function centroids = kMeansInitCentroids(X, K)
+%KMEANSINITCENTROIDS This function initializes K centroids that are to be 
+%used in K-Means on the dataset X
+%   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
+%   used with the K-Means on the dataset X
+%
+
+% You should return this values correctly
+centroids = zeros(K, size(X, 2));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should set centroids to randomly chosen examples from
+%               the dataset X
+%
+
+% Get random K sample from X
+p = randperm(size(X,1),K);
+% Let these random K sample become centroids
+centroids = X(p,:);
+
+
+
+
+
+
+% =============================================================
+
+end
+
